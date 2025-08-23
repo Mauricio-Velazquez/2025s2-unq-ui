a68192f2740a9c2632e78c5baadf3f8834fd84fd
Franco <francofumiere321@gmail.com>
2025-08-23 16:16:17 -0300

    agrego manejo de error por mal formato de json

--- Api/src/main.js
+++ Api/src/main.js
@@ -3,13 +3,17 @@ import { authRouter } from './auth/auth.routes.js'
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded())
 
 app.use('/', authRouter)
 
 app.use('/', (err, req, res, next) => {
   console.error(err)
 
-  return res.status(500).json({ message: 'Internal Server Error' })
+  const statusCode = err.statusCode || 500
+  const message = statusCode === 500 ? 'Internal Server Error' : 'Bad Request'
+
+  return res.status(statusCode).json({ message })
 })
 
 const PORT = 8080
