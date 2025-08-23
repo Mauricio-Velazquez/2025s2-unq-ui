cb51da4b031a14e0b6c7a5bd88d9a204f6547498
Franco <francofumiere321@gmail.com>
2025-08-23 16:33:54 -0300

    agrego DTOs y validaciones para rutas de auth

--- Api/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function getUserFromReq (req) {
-  const token = getTokenFromReq(req)
-  const user = this.system.getUser(token.id)
-  return user
-}
-
-export function getTokenFromReq (req) {
-
-}
