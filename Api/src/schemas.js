fdf74614ee137c9d12dc1dee9474af4702562956
Franco Fumiere <francofumiere321@gmail.com>
2025-08-25 22:06:31 -0300

    implemento a medias ruta para crear posts

---
+++ Api/src/schemas.js
@@ -0,0 +1,13 @@
+import { object, string } from 'yup'
+
+export const putPostBodySchema = object({
+  image: string().url().required(),
+  body: string().required()
+})
+  .noUnknown(true)
+  .strict()
+
+export const postBodySchema = object({
+  image: string().url().trim().required(),
+  description: string().required()
+})
