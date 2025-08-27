3ca63c31c3d3932d6aed62e534f2d98742c9e987
Franco <francofumiere321@gmail.com>
2025-08-27 19:31:23 -0300

    delego la validacion y casteo del body al middleware checkBody en las rutas de post

--- Api/src/routers/posts.routes.js
+++ Api/src/routers/posts.routes.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import { ROLES } from '../config/constants.js'
+import { checkBody } from '../middleware/checkBody.js'
+import { postBodySchema } from '../schemas/schemas.js'
 
 const createPostsRouter = (postsController, tokenController) => {
   const postsRouter = express.Router()
 
-  postsRouter.post('/', tokenController.checkRole(ROLES.USER), postsController.createPost)
+  postsRouter.post('/', checkBody(postBodySchema), tokenController.checkRole(ROLES.USER), postsController.createPost)
 
   postsRouter.get(
     '/:postId',
     tokenController.checkRole(ROLES.PUBLIC),
     postsController.getPost
   )
+
+  postsRouter.put('/:postId', checkBody(postBodySchema), tokenController.checkRole(ROLES.USER), postsController.editPost)
 
   return postsRouter
 }
