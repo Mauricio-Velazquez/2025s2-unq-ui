8162b9b1cf5e212f992a1714b1a7b927ccc3855d
Franco Fumiere <francofumiere321@gmail.com>
2025-08-30 15:52:15 -0300

    refactor

--- Api/src/index.js
+++ Api/src/index.js
@@ -1,28 +1,28 @@
 import express from 'express'
 import searchRouter from './routers/search.routes.js'
-import getInstagramSystem from '@unq-ui/instagram-model-js'
 import createPostsRouter from './routers/posts.routes.js'
 import PostsController from './controllers/posts.controller.js'
 import { authRouter } from './routers/auth.routes.js'
 import TokenController from './controllers/token.controller.js'
 import createUserRouter from './routers/user.routes.js'
 import UserController from './controllers/user.controller.js'
+import { instagramSystem } from './services/instagramSystem.js'
 
-const system = getInstagramSystem()
-const tokenController = new TokenController(system)
-const postsController = new PostsController(system, tokenController)
-const userController = new UserController(system)
+const tokenController = new TokenController(instagramSystem)
+const postsController = new PostsController(instagramSystem, tokenController)
+const userController = new UserController(instagramSystem)
 const app = express()
 const port = 7070
+const postsRouter = createPostsRouter(postsController, tokenController)
+const usersRouter = createUserRouter(userController, tokenController)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/search', searchRouter)
-app.use('/posts', createPostsRouter(postsController, tokenController))
+app.use('/posts', postsRouter)
 app.use('/', authRouter)
-app.use('/user', createUserRouter(userController, tokenController))
-app.use('/users', createUserRouter(userController, tokenController))
+app.use('/users', usersRouter)
 
 app.use((err, req, res, next) => {
   if (err.constructor === Error) {
