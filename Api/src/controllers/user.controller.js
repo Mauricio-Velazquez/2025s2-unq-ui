13a4c2e1436b785b9b5577e9c1f4b85f9d875f76
Franco Fumiere <francofumiere321@gmail.com>
2025-08-30 13:41:14 -0300

    cambio nombre de archivos

---
+++ Api/src/controllers/user.controller.js
@@ -0,0 +1,67 @@
+import { transformUserPosts, transformUserTimeline } from '../utils/Dtos.js'
+
+class UserController {
+  constructor (system) {
+    this.system = system
+  }
+
+  getUserTimeline = (req, res) => {
+    const loggedUserId = req.user.id
+    const user = this.system.getUser(loggedUserId)
+    const timeline = this.system.timeline(loggedUserId)
+
+    const userDto = transformUserTimeline({ ...user, timeline })
+    res.status(200).json(userDto)
+  }
+
+  getUser = (req, res) => {
+    try {
+      const userId = req.params.userId
+      const user = this.system.getUser(userId)
+      const posts = this.system.getPostByUserId(userId)
+
+      const userDto = transformUserPosts({ ...user, posts })
+
+      res.json(userDto)
+    } catch (error) {
+      res.status(404).send(`Usuario ${req.params.userId} no encontrado`)
+    }
+  }
+
+  putUserFollow = (req, res) => {
+    const { userId } = req.params
+    const loggedUserId = req.user.id
+
+    if (userId === loggedUserId) {
+      return res.status(400).json({ error: "Can't add yourself as a friend" })
+    }
+
+    let targetUser
+    let loggedUser
+
+    try {
+      targetUser = this.system.getUser(userId)
+      loggedUser = this.system.getUser(loggedUserId)
+    } catch (error) {
+      return res.status(404).json({ error: 'Not found' })
+    }
+
+    const alreadyFollower = targetUser.followers.some(f => f.id === loggedUser.id)
+
+    if (alreadyFollower) {
+      targetUser.followers = targetUser.followers.filter(f => f.id !== loggedUser.id)
+    } else {
+      targetUser.followers.push({
+        id: loggedUser.id,
+        name: loggedUser.name,
+        image: loggedUser.image
+      })
+    }
+
+    const posts = this.system.getPostByUserId(userId)
+
+    res.json(transformUserPosts({ ...targetUser, posts }))
+  }
+}
+
+export default UserController
--- Api/src/index.js
+++ Api/src/index.js
@@ -5,13 +5,13 @@ import createPostsRouter from './routers/posts.routes.js'
 import PostsController from './controllers/posts.controller.js'
 import { authRouter } from './routers/auth.routes.js'
 import TokenController from './controllers/token.controller.js'
-import createUserRouter from "./routers/userRouter.js";
-import UserController from "./controllers/userController.js";
+import createUserRouter from './routers/user.routes.js'
+import UserController from './controllers/user.controller.js'
 
 const system = getInstagramSystem()
 const tokenController = new TokenController(system)
 const postsController = new PostsController(system, tokenController)
-const userController = new UserController(system);
+const userController = new UserController(system)
 const app = express()
 const port = 7070
 
@@ -21,8 +21,8 @@ app.use(express.json())
 app.use('/search', searchRouter)
 app.use('/posts', createPostsRouter(postsController, tokenController))
 app.use('/', authRouter)
-app.use("/user", createUserRouter(userController, tokenController));
-app.use("/users", createUserRouter(userController, tokenController));
+app.use('/user', createUserRouter(userController, tokenController))
+app.use('/users', createUserRouter(userController, tokenController))
 
 app.use((err, req, res, next) => {
   if (err.constructor === Error) {
@@ -37,5 +37,3 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
-console.log(system.users)
