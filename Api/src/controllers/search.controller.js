630d36814ef0ac96d3dd6edfe55e87226fcf1ed0
Franco Fumiere <francofumiere321@gmail.com>
2025-09-01 22:23:09 -0300

    arreglo mezcla de idiomas

--- Api/src/controllers/search.controller.js
+++ Api/src/controllers/search.controller.js
@@ -1,23 +1,23 @@
-import { transformUserSimple, transformPost, transformComment } from '../utils/Dtos.js'
+import { transformUserSimple, transformPost } from '../utils/Dtos.js'
 
 class SearchController {
-  constructor(system) {
+  constructor (system) {
     this.system = system
   }
 
   search = (req, res) => {
     const { query } = req.query
-    if (!query) return res.status(400).json({ message: 'Falta el parámetro query' })
+    if (!query) return res.status(400).json({ message: 'query parameter missing' })
 
-    //Busca usuarios
+    // Busca usuarios
     const users = this.system.users
       .filter(user => user.name.toLowerCase().includes(query.toLowerCase()))
       .map(transformUserSimple)
 
-    //Busca posts
+    // Busca posts
     const posts = this.system.posts
       .filter(post => post.description.toLowerCase().includes(query.toLowerCase()) ||
-      post.user.name.toLowerCase().includes(query.toLowerCase()))  
+      post.user.name.toLowerCase().includes(query.toLowerCase()))
       .map(transformPost)
 
     return res.status(200).json({ users, posts })
